e5bd74f592345c3c268a8bac9111671d5c1e3455
balthazar <balthazar.supiot@epitech.eu>
2023-10-09 15:25:19 +0200

    push learn More

---
+++ script.js
@@ -0,0 +1,8 @@
+function LearnMore() {
+    var x = document.getElementById("description");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
