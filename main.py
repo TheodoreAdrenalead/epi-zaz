dd7b08c1349f4bb123a986760303f7f60f4cffab
balthazar <balthazar.supiot@epitech.eu>
2023-10-10 18:33:09 +0200

    push

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import mysql.connector
 app = FastAPI()
 
 mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="balthazar@localhost",
-  password="chillflex",
-  database="data_base.sql"
+host="127.0.0.1",
+user="balthazar",
+password="chillflex",
+database="data_base"
 )
 
 # get line from table by id 
@@ -18,7 +18,7 @@ def get_table(table : str, id : int):
     result = cursor.fetchone()
     return {result}
 
-# Delete an line from table by ID
+# # Delete an line from table by ID
 @app.delete("/tableLine")
 def delete_people(table :str, id: int):
     cursor = mydb.cursor()
@@ -27,7 +27,7 @@ def delete_people(table :str, id: int):
     return {"message": "Employee deleted successfully"}
 
 
-# Add a new employee
+ # Add a new employee
 @app.post("/people")
 def add_people(firstName : str, lastName: str, status : str, tel : str, email : str ,mdp : str):
     cursor = mydb.cursor()
@@ -38,13 +38,12 @@ def add_people(firstName : str, lastName: str, status : str, tel : str, email : 
     return True
 
 
-# Add a new advertisement
+# # Add a new advertisement
 @app.post("/advertisement")
 def add_people(title : str, shortDescription: str, detailDescription: str):
-    cursor = mydb.cursor()
-    sql = "INSERT INTO employees (title, shortDescription, detailDescription) VALUES (%s, %s, %s)"
-    val = (title, shortDescription, detailDescription)
-    cursor.execute(sql, val)
-    mydb.commit()
-    return True
-
+   cursor = mydb.cursor()
+   sql = "INSERT INTO employees (title, shortDescription, detailDescription) VALUES (%s, %s, %s)"
+   val = (title, shortDescription, detailDescription)
+   cursor.execute(sql, val)
+   mydb.commit()
+   return True
--- script.js
+++ script.js
@@ -6,3 +6,58 @@ function LearnMore(element) {
         x.style.display = "none";
     }
 } 
+
+function connectPage(){
+    // redirect connection page
+    window.location.href = "connectPage.html";
+   
+}
+
+function connectForm(){
+    form = document.getElementById("connectForm");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+function createForm(){
+    form = document.getElementById("createForm");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+
+function homePage(){
+    // redirect connection page
+    window.location.href = "job_ad.html";
+}
+
+
+function connect(element) {
+    form = document.getElementById("connectForm");
+    emailTest = form.email.value;
+    mdpTest = form.mdp.value;
+
+ 
+} 
+function createAccount(element) {
+   form = document.getElementById("createForm")
+   firstName = form.firstName.value;
+   lastName = form.lastName.value;
+   statu = form.statu.value;
+   tel = form.tel.value;
+   mdp1 = form.mdp1.value;
+   mdp2=form.mdp2.value;
+   if(mdp1!=mdp2){
+    alert("Mauvais mot de passe");
+   }
+   else{
+    mdp = mdp1;
+   }
+
+ 
+} 
